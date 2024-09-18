729d7cd3fa0527ff009e89074b7cbfea84a61159
naokkitam <na69ok@gmail.com>
2024-09-18 21:19:38 +0900

    kadai_005フォルダの追加

---
+++ kadai_005/score.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題005</title>
+</head>
+<body>
+  <?php
+    $score01 = '80';
+    $score02 = '60';
+    $score03 = '55';
+    $score04 = '40';
+    $score05 = '100';
+    $score06 = '25';
+    $score07 = '80';
+    $score08 = '95';
+    $score09 = '30';
+    $score10 = '60';
+
+    $total = $score01 + $score02 + $score03 + $score04 + $score05 +  $score06 + $score07 + $score08 + $score09 + $score10;
+    echo $total; 
+    echo '<br>';
+    echo $total / 10;
+    ?>
+
+</body>
+</html>
