6ed34fb48fb79abca297484aa89e9f5246e4b6f3
naokkitam <na69ok@gmail.com>
2024-09-22 16:16:38 +0900

    kadai_007フォルダの追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題007</title>
+</head>
+<body>
+  <p>
+    <?php
+    $vegetables = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($vegetables);
+    ?>
+  </p>
+</body>
+</html>
